f530ea15af7cd55f22419efdbb1d64222f88fa9f
ksyxb <729902288@qq.com>
2024-06-25 08:21:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="wrapper">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+  <RouterView />
+</template>
+
